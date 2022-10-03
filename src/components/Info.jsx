33ab7dd48b6b7eb3a1d180424282a13a2b4bc973
tonyclem <tonyclem001@gmail.com>
2022-10-03 07:15:23 +0200

    added navbar discount and info compontent

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -5,15 +5,46 @@ import { BsTelephone } from "react-icons/bs";
 const InfoContent = () => {
   return (
     <Wrapper>
-      <BsTelephone />
-      <h4>+3160000000</h4>
+      <hr />
+      <div>
+        <span className="content-help">
+          <BsTelephone />
+          <h5>Help</h5>
+          <h4>+3160000000</h4>
+        </span>
+        <hr />
+        <span>Login</span>
+        <hr />
+        <span>Register</span>
+      </div>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.div`
-  display: flex;
-  justify-content: end;
+  div {
+    display: flex;
+    justify-content: end;
+    margin-right: 2.5rem;
+
+    span {
+      margin: 9px;
+    }
+
+    .content-help {
+      display: flex;
+      h5 {
+        padding: 0 13px;
+      }
+      h4 {
+        padding-right: 10px;
+      }
+    }
+  }
+
+  @media (max-width: 600px) {
+    font-size: 12px;
+  }
 `;
 
 export default InfoContent;
--- src/components/NavBarDiscount.js
+++ src/components/NavBarDiscount.js
@@ -1,18 +1,25 @@
 import React from "react";
 import styled from "styled-components";
+import { BsXLg } from "react-icons/bs";
 
 const NavBarDiscount = () => {
   return (
-    <Wrapper className="container  ">
+    <Wrapper className="container">
       <h4>Save 30% - 60% in each orders you purchase</h4>
-      <hr />
+      <BsXLg />
     </Wrapper>
   );
 };
 
 const Wrapper = styled.div`
-  .btn {
-    margin-left: 15rem;
+  display: flex;
+  text-align: center;
+  justify-content: space-around;
+  margin: 0.8rem;
+  font-size: 1.2rem;
+
+  @media (max-width: 600px) {
+    font-size: 10px;
   }
 `;
 
