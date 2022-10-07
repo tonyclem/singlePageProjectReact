4e620f06da294d47481255b9d7e9d1f738a67dcd
tonyclem <tonyclem001@gmail.com>
2022-10-07 23:22:29 +0200

    added new product functionality

--- src/components/NewProduct.jsx
+++ src/components/NewProduct.jsx
@@ -1,12 +1,48 @@
 import React from "react";
 import styled from "styled-components";
 import { carouseImages } from "../utils/Constants";
+import { BsStar, BsHeart } from "react-icons/bs";
+import images from "../assets/fashion-logo.webp";
 
 const NewProduct = () => {
   return (
     <Wrapper>
-      <div>
-        <h1> New Product</h1>
+      <div className="container">
+        <h1> New Products</h1>
+        <hr />
+        <div className="container-inner">
+          <div className="images-container">
+            <img src={images} alt="name" />
+            <div className="icon-Container">
+              <span className="icon">
+                <p>
+                  <BsHeart />
+                </p>
+              </span>
+              <span className="icon">
+                <p>
+                  <BsHeart />
+                </p>
+              </span>
+              <span className="icon">
+                <p>
+                  <BsHeart />
+                </p>
+              </span>
+            </div>
+            <div className="footer">
+              <h6>$55</h6>
+              <div className="btn-icon">
+                <BsStar />
+                <BsStar />
+                <BsStar />
+                <BsStar />
+                <BsStar />
+              </div>
+              <p>smile fashion</p>
+            </div>
+          </div>
+        </div>
       </div>
     </Wrapper>
   );
@@ -15,6 +51,65 @@ const NewProduct = () => {
 const Wrapper = styled.div`
   margin: 3rem 2rem;
   background-color: #f1f1f1;
+  height: 35vw;
+
+  .container {
+    margin: 2rem 1.5rem;
+    width: 100%;
+
+    h1 {
+      padding: 1.2rem 0;
+    }
+
+    hr {
+      margin-bottom: 1rem;
+      width: 95%;
+    }
+
+    .container-inner {
+      border-radius: 2px;
+      .images-container {
+        width: 200px;
+        height: 320px;
+        border: 1px solid #b3b3b3;
+        border-radius: 2px;
+        position: relative;
+
+        img {
+          width: 199px;
+          height: 220px;
+        }
+
+        .icon {
+          display: block;
+          color: #000;
+          background-color: #fff;
+          border-radius: 50%;
+          width: 2rem;
+          height: 2rem;
+          margin: auto;
+          align-items: center;
+          top: 5%;
+          position: absolute;
+          p {
+            padding: 0.5rem;
+          }
+        }
+
+        .footer {
+          padding: 10px;
+
+          .btn-icon {
+            padding: 0.4rem 0;
+          }
+        }
+      }
+
+      .images-container:hover {
+        box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+      }
+    }
+  }
 `;
 
 export default NewProduct;
